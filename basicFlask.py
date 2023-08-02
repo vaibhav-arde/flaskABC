fbeb8dcf20b46b2d2158afcf585082b8a9822d74
vaibhav-arde <vaibhav.arde@gmail.com>
2023-08-02 07:59:25 +0530

    Connected apis to form

--- basicFlask.py
+++ basicFlask.py
@@ -6,6 +6,28 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+@app.route('/math', methods=["POST"])
+def calculate():
+    print(str(request.form))
+    operation = request.form["operation"]
+    num1 = int(request.form["num1"])
+    num2 = int(request.form["num2"])
+    if operation== "add":
+        result = num1 + num2
+        addResult =f"Sum of {num1} + {num2} is {result}"
+    elif operation== "subtract":
+        result = abs(num1 - num2)
+        addResult =f"Subtraction of {num1} - {num2} is {result}"
+    elif operation== "multiply":
+        result = num1 * num2
+        addResult =f"Multiplication of {num1} * {num2} is {result}"
+    elif operation== "divide":
+        result = num1 / num2
+        addResult =f"Division of {num1} / {num2} is {result}"
+    else:
+        addResult =f"{operation} is either not arithmatic or its not supported."
+    return render_template('results.html',result = addResult)
+
 @app.route("/")
 def hello_world():
     return "<h1>Hello Home<h1>"
