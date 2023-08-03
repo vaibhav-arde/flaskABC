db3eb154b8c81f201e0fbaf9778a6165a1a4f0fa
vaibhav-arde <vaibhav.arde@gmail.com>
2023-08-03 07:31:00 +0530

    testing code with postman

--- basicFlask.py
+++ basicFlask.py
@@ -28,6 +28,33 @@ def calculate():
         addResult =f"{operation} is either not arithmatic or its not supported."
     return render_template('results.html',result = addResult)
 
+@app.route('/mathWithPostman', methods=["POST", "GET"])
+def calculateWithPostman():
+    if request.method=="POST":
+        print(str(request.json))
+        operation = request.json["operation"]
+        num1 = int(request.json["num1"])
+        num2 = int(request.json["num2"])
+    if request.method=="GET":
+        operation = request.args.get('operation')
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+    if operation== "add":
+        result = num1 + num2
+        addResult =f"Sum of {num1} + {num2} is {result}"
+    elif operation== "subtract":
+        result = abs(num1 - num2)
+        addResult =f"Subtraction of {num1} - {num2} is {result}"
+    elif operation== "multiply":
+        result = num1 * num2
+        addResult =f"Multiplication of {num1} * {num2} is {result}"
+    elif operation== "divide":
+        result = num1 / num2
+        addResult =f"Division of {num1} / {num2} is {result}"
+    else:
+        addResult =f"{operation} is either not arithmatic or its not supported."
+    return jsonify(addResult)
+
 @app.route("/")
 def hello_world():
     return "<h1>Hello Home<h1>"
